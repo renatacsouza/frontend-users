9c70cead6332a32045be40064fc646dbea3cb1d8
Renata Souza <renata.souza@neon.br>
2024-05-08 18:16:02 -0300

    feat(header): add new header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend-users';
