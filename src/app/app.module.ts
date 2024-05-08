959d539243586e3fbd6686724f56674d443625f7
Renata Souza <renata.souza@neon.br>
2024-05-08 18:46:32 -0300

    feat(logged area): add logged area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoggedUserComponent } from './logged-user/logged-user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, LoggedUserComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/logged-user/logged-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-user',
+  templateUrl: './logged-user.component.html',
+  styleUrl: './logged-user.component.scss'
+})
+export class LoggedUserComponent {
+
+}
